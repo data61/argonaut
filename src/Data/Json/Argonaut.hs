035864bef61352e649aac88cfad643c6095f7d4c
Tony Morris <tmorris@tmorris.net>
2016-10-06 20:48:43 +1000

    argonaut

--- src/Data/Json/Argonaut.hs
+++ src/Data/Json/Argonaut.hs
@@ -1,2 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
 module Data.Json.Argonaut where
-  
+
+import Data.Text
+import Data.Text1
+import Papa
+
+data JsonAssoc s =
+  JsonAssoc {
+    _key ::
+      Text
+  , _value ::
+      Json s
+  }
+  deriving (Eq, Ord, Show)
+
+newtype JsonAssocs s =
+  JsonAssocs
+    [JsonAssoc s]
+  deriving (Eq, Ord, Show)
+
+data Json s =
+  JsonNull s
+  | JsonBool Bool s
+  | JsonNumber Text1 s
+  | JsonString Text s
+  | JsonArray (Jsons s) s
+  | JsonObject (JsonAssocs s) s
+  deriving (Eq, Ord, Show)
+
+newtype Jsons s =
+  Jsons
+    [Json s]
+  deriving (Eq, Ord, Show)
+
+makeClassy ''JsonAssoc
+makeWrapped ''JsonAssocs
+makeClassy ''Json
+makeClassyPrisms ''Json
+makeWrapped ''Jsons
