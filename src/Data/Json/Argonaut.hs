f535ddb003e365d39f4a8c4e5292ed15bc2cecab
Tony Morris <tmorris@tmorris.net>
2016-10-25 14:53:25 +1000

    json parsers

--- src/Data/Json/Argonaut.hs
+++ src/Data/Json/Argonaut.hs
@@ -6,11 +6,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 
+{-# LANGUAGE RankNTypes #-}
+
 module Data.Json.Argonaut where
 
-import Control.Applicative((<|>))
+import Control.Applicative as Applicative((*>))
+import Control.Applicative(Alternative((<|>), many))
 import Data.Foldable(asum)
 import Text.Parser.Char
+import Text.Parser.Combinators
 import Data.Text(Text)
 import Papa
 
@@ -18,13 +22,15 @@ import Papa
 -- >>> :set -XNoImplicitPrelude
 -- >>> :set -XFlexibleContexts
 -- >>> :set -XOverloadedStrings
+-- >>> import Control.Applicative as Applicative((<*))
 -- >>> import Data.Either(isLeft)
 -- >>> import Data.Text(pack)
 -- >>> import Data.Text.Arbitrary
 -- >>> import Text.Parsec(Parsec, ParseError, parse)
 -- >>> import Test.QuickCheck(Arbitrary(..))
 -- >>> let testparse :: Parsec Text () a -> Text -> Either ParseError a; testparse p = parse p "test"
-
+-- >>> let testparsetheneof :: Parsec Text () a -> Text -> Either ParseError a; testparsetheneof p = testparse (p Applicative.<* eof)
+-- >>> let testparsethennoteof :: Parsec Text () a -> Text -> Either ParseError a; testparsethennoteof p = testparse (p Applicative.<* anyChar)
 
 ----
 
@@ -153,7 +159,7 @@ instance AsJCharUnescaped Char where
                   Just (JCharUnescaped c)
       )
 
-data JChar =
+data JCharEscaped =
   QuotationMark
   | ReverseSolidus
   | Solidus
@@ -163,7 +169,11 @@ data JChar =
   | CarriageReturn
   | Tab
   | Hex HexDigit4
-  | Unescaped JCharUnescaped
+  deriving (Eq, Ord, Show)
+
+data JChar =
+  EscapedJChar JCharEscaped
+  | UnescapedJChar JCharUnescaped
   deriving (Eq, Ord, Show)
 
 newtype JString =
@@ -212,6 +222,8 @@ newtype Jsons s =
 makeClassy ''HexDigit
 makeClassyPrisms ''HexDigit
 makeClassy ''HexDigit4
+makeClassy ''JCharEscaped
+makeClassyPrisms ''JCharEscaped
 makeClassy ''JChar
 makeClassyPrisms ''JChar
 makeWrapped ''JString
@@ -230,6 +242,12 @@ makeWrapped ''Jsons
 -- >>> testparse (parseJsonNull (return ())) "null" 
 -- Right (JsonNull ())
 --
+-- >>> testparsetheneof (parseJsonNull (return ())) "null" 
+-- Right (JsonNull ())
+--
+-- >>> testparsethennoteof (parseJsonNull (return ())) "nullx" 
+-- Right (JsonNull ())
+--
 -- prop> x /= "null" ==> isLeft (testparse (parseJsonNull (return ())) x)
 parseJsonNull ::
   CharParsing f =>
@@ -244,6 +262,18 @@ parseJsonNull p =
 -- Right (JsonBool True ())
 --
 -- >>> testparse (parseJsonBool (return ())) "false" 
+-- Right (JsonBool False ())
+---
+-- >>> testparsetheneof (parseJsonBool (return ())) "true" 
+-- Right (JsonBool True ())
+--
+-- >>> testparsetheneof (parseJsonBool (return ())) "false" 
+-- Right (JsonBool False ())
+---
+-- >>> testparsethennoteof (parseJsonBool (return ())) "truex" 
+-- Right (JsonBool True ())
+--
+-- >>> testparsethennoteof (parseJsonBool (return ())) "falsex" 
 -- Right (JsonBool False ())
 --
 -- prop> (x `notElem` ["true", "false"]) ==> isLeft (testparse (parseJsonBool (return ())) x)
@@ -277,6 +307,12 @@ parseJsonBool p =
 --
 -- >>> testparse parseHexDigit "F" 
 -- Right F
+--
+-- >>> testparsetheneof parseHexDigit "F" 
+-- Right F
+--
+-- >>> testparsethennoteof parseHexDigit "Fx" 
+-- Right F
 parseHexDigit ::
   CharParsing f =>
   f HexDigit
@@ -307,10 +343,155 @@ parseHexDigit =
     , DF <$ char 'F'
     ]
 
+-- |
+--
+-- >>> testparse parseHexDigit4 "1234"
+-- Right 1234
+--
+-- >>> testparse parseHexDigit4 "12aF"
+-- Right 12aF
+--
+-- >>> testparse parseHexDigit4 "aBcD"
+-- Right aBcD
+--
+-- >>> testparsetheneof parseHexDigit4 "12aF"
+-- Right 12aF
+--
+-- >>> testparsethennoteof parseHexDigit4 "12aFx"
+-- Right 12aF
 parseHexDigit4 ::
   CharParsing f =>
   f HexDigit4
 parseHexDigit4 =
   HexDigit4 <$> parseHexDigit <*> parseHexDigit <*> parseHexDigit <*> parseHexDigit
 
--- parseJCharUnescaped, parseJChar
+-- |
+--
+-- >>> testparse parseJCharUnescaped "a" 
+-- Right (JCharUnescaped 'a')
+--
+-- >>> testparse parseJCharUnescaped "\8728" 
+-- Right (JCharUnescaped '\8728')
+--
+-- >>> testparsetheneof parseJCharUnescaped "a" 
+-- Right (JCharUnescaped 'a')
+--
+-- >>> testparsethennoteof parseJCharUnescaped "ax" 
+-- Right (JCharUnescaped 'a')
+parseJCharUnescaped ::
+  CharParsing f =>
+  f JCharUnescaped
+parseJCharUnescaped =
+  JCharUnescaped <$> satisfy (has _JCharUnescaped)
+
+-- |
+--
+-- >>> testparse parseJCharEscaped "\\\""
+-- Right QuotationMark
+--
+-- >>> testparse parseJCharEscaped "\\\\"
+-- Right ReverseSolidus
+--
+-- >>> testparse parseJCharEscaped "\\/"
+-- Right Solidus
+--
+-- >>> testparse parseJCharEscaped "\\b"
+-- Right Backspace
+--
+-- >>> testparse parseJCharEscaped "\\f"
+-- Right FormFeed
+--
+-- >>> testparse parseJCharEscaped "\\n"
+-- Right LineFeed
+--
+-- >>> testparse parseJCharEscaped "\\r"
+-- Right CarriageReturn
+--
+-- >>> testparse parseJCharEscaped "\\t"
+-- Right Tab
+--
+-- >>> testparse parseJCharEscaped "\\u1234"
+-- Right (Hex 1234)
+--
+-- >>> testparsetheneof parseJCharEscaped "\\t"
+-- Right Tab
+--
+-- >>> testparsethennoteof parseJCharEscaped "\\tx"
+-- Right Tab
+parseJCharEscaped ::
+  CharParsing f =>
+  f JCharEscaped
+parseJCharEscaped =
+  let e =
+        asum
+          ((\(c, p) -> char c Applicative.*> pure p) <$>
+            [
+              ('"' , QuotationMark)
+            , ('\\', ReverseSolidus)
+            , ('/' , Solidus)
+            , ('b' , Backspace)
+            , ('f' , FormFeed)
+            , ('n' , LineFeed)
+            , ('r' , CarriageReturn)
+            , ('t' , Tab)
+            ])
+      h =
+        Hex <$> (char 'u' Applicative.*> parseHexDigit4)
+  in  char '\\' Applicative.*> (e <|> h)
+
+-- |
+--
+-- >>> testparse parseJChar "a"
+-- Right (UnescapedJChar (JCharUnescaped 'a'))
+--
+-- >>> testparse parseJChar "\\u1234"
+-- Right (EscapedJChar (Hex 1234))
+--
+-- >>> testparse parseJChar "\\r"
+-- Right (EscapedJChar CarriageReturn)
+--
+-- >>> testparsetheneof parseJChar "a"
+-- Right (UnescapedJChar (JCharUnescaped 'a'))
+--
+-- >>> testparsethennoteof parseJChar "ax"
+-- Right (UnescapedJChar (JCharUnescaped 'a'))
+parseJChar ::
+  CharParsing f =>
+  f JChar
+parseJChar =
+  asum
+    [
+      EscapedJChar <$> try parseJCharEscaped
+    , UnescapedJChar <$> parseJCharUnescaped
+    ]
+
+-- |
+--
+-- >>> testparse parseJString ""
+-- Right (JString [])
+--
+-- >>> testparse parseJString "abc"
+-- Right (JString [UnescapedJChar (JCharUnescaped 'a'),UnescapedJChar (JCharUnescaped 'b'),UnescapedJChar (JCharUnescaped 'c')])
+--
+-- >>> testparse parseJString "a\\rbc"
+-- Right (JString [UnescapedJChar (JCharUnescaped 'a'),EscapedJChar CarriageReturn,UnescapedJChar (JCharUnescaped 'b'),UnescapedJChar (JCharUnescaped 'c')])
+--
+-- >>> testparse parseJString "a\\rbc\\uab12\\ndef\\\""
+-- Right (JString [UnescapedJChar (JCharUnescaped 'a'),EscapedJChar CarriageReturn,UnescapedJChar (JCharUnescaped 'b'),UnescapedJChar (JCharUnescaped 'c'),EscapedJChar (Hex ab12),EscapedJChar LineFeed,UnescapedJChar (JCharUnescaped 'd'),UnescapedJChar (JCharUnescaped 'e'),UnescapedJChar (JCharUnescaped 'f'),EscapedJChar QuotationMark])
+--
+-- >>> testparsetheneof parseJString ""
+-- Right (JString [])
+--
+-- >>> testparsetheneof parseJString "abc"
+-- Right (JString [UnescapedJChar (JCharUnescaped 'a'),UnescapedJChar (JCharUnescaped 'b'),UnescapedJChar (JCharUnescaped 'c')])
+--
+-- >>> testparsethennoteof parseJString "a\\u"
+-- Right (JString [UnescapedJChar (JCharUnescaped 'a')])
+--
+-- >>> testparsethennoteof parseJString "a\t"
+-- Right (JString [UnescapedJChar (JCharUnescaped 'a')])
+parseJString ::
+  CharParsing f =>
+  f JString
+parseJString =
+  JString <$> many parseJChar
